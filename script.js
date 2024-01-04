e420bd2db1bd59c47e089ac5e205b6124a437a88
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-01-05 00:52:20 +0600

    Initial commit

--- script.js
+++ script.js
@@ -1,47 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const adviceParagraph = document.querySelector('.advice-paragraph');
-    const adviceGeneratorButton = document.querySelector('.advice-generator');
-    const headerElement = document.querySelector('.header');
+const dice = document.getElementById("dice");
+const adviceNum = document.getElementById("advice-number");
+const adviceText = document.querySelector(".advice-text");
 
-    let adviceNumber = 1; // Initial advice number
+window.onload = showQuote;
 
-    function generateRandomAdvice() {
-        fetch('https://api.adviceslip.com/advice')
-            .then(response => response.json())
-            .then(data => {
-                const advice = data.slip.advice;
-                adviceParagraph.textContent = `"${advice}"`;
-                adviceNumber++;
-                headerElement.textContent = `Advice #${adviceNumber}`;
-            })
-            .catch(error => console.error('Error:', error));
-    }
-    adviceGeneratorButton.addEventListener('click', generateRandomAdvice);
+dice.addEventListener("click", () => {
+    showQuote();
 });
 
-
-
-/*
-// Without API
-document.addEventListener('DOMContentLoaded', function () {
-    const adviceParagraph = document.querySelector('.advice-paragraph');
-    const adviceGeneratorButton = document.querySelector('.advice-generator');
-    const headerElement = document.querySelector('.header');
-
-    let adviceNumber = 1; // Initial advice number
-
-    const adviceList = [
-        "It is easy to sit up and take notice, what's difficult is getting up and taking action.",
-        "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
-        "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle."
-    ];
-
-    function generateRandomAdvice() {
-        const randomIndex = Math.floor(Math.random() * adviceList.length);
-        adviceParagraph.textContent = `"${adviceList[randomIndex]}"`;
-        adviceNumber++;
-        headerElement.textContent = `Advice #${adviceNumber}`;
-    }
-    adviceGeneratorButton.addEventListener('click', generateRandomAdvice);
-});
-*/
+function showQuote() {
+    fetch("https://api.adviceslip.com/advice")
+        .then(response => response.json())
+        .then((data) => data.slip)
+        .then((data) => {
+            adviceNum.textContent = data.id;
+            adviceText.textContent = data.advice;
+        })
+        .catch((error) => {
+            alert(`Error ${error}`);
+        });
+}
